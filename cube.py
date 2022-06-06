94dedb8070011897c1a47b1098b16a6487aa1bc1
kotyaro508 <kotliarov.iad@phystech.edu>
2022-06-07 00:03:56 +0300

    Create motion without control

--- cube.py
+++ cube.py
@@ -8,11 +8,11 @@ class Box:
     def __init__(self, center, sizes, orientation):
         self.center = center
         self.sizes = sizes
-        self.matrix = R.from_euler('xyz', orientation).as_matrix()
-    
+        self.matrix = R.from_euler('xyz', orientation).as_matrix().T
+
     def box_edges(self):
         edges = []
-        
+
         for j in (-1, 1):
             for k in (-1, 1):
                 edges.append(np.array([[-self.sizes[0],
@@ -20,8 +20,8 @@ class Box:
                                         k * self.sizes[2]],
                                        [self.sizes[0],
                                         j * self.sizes[1],
-                                        k * self.sizes[2]]]).T / 2)
-        
+                                        k * self.sizes[2]]]).T)
+
         for k in (-1, 1):
             for i in (-1, 1):
                 edges.append(np.array([[i * self.sizes[0],
@@ -29,8 +29,8 @@ class Box:
                                         k * self.sizes[2]],
                                        [i * self.sizes[0],
                                         self.sizes[1],
-                                        k * self.sizes[2]]]).T / 2)
-        
+                                        k * self.sizes[2]]]).T)
+
         for i in (-1, 1):
             for j in (-1, 1):
                 edges.append(np.array([[i * self.sizes[0],
@@ -38,35 +38,37 @@ class Box:
                                         -self.sizes[2]],
                                        [i * self.sizes[0],
                                         j * self.sizes[1],
-                                        self.sizes[2]]]).T / 2)
-        
-        edges = np.array(edges)
-        
+                                        self.sizes[2]]]).T)
+
+        edges = np.array(edges) / 2
+
         for i in range(12):
             edges[i] = self.matrix.T @ edges[i] + np.array([self.center, self.center]).T
-        
+
         return edges
-                    
-    # def render(self, ax):
-    #     colors = ['red', 'blue', 'green']
-    #     static_axes = np.eye(3)
-    #
-    #
-    #     lines = []
-    #     for i in range(3):
-    #         # fixed coordinate system
-    #         vector = np.array([self.center, self.center + static_axes[i]]).T
-    #         lines.append(ax.plot(vector[0], vector[1], vector[2], color=colors[i])[0])
-    #
-    #         # coordinate system associated with the body
-    #         vector = np.array([self.center, self.center + self.matrix[i]]).T
-    #         lines.append(ax.plot(vector[0], vector[1], vector[2], color=colors[i])[0])
-    #
-    #     # body
-    #     for edge in self.box_edges():
-    #         lines.append(ax.plot(edge[0], edge[1], edge[2], color='grey')[0])
-    #
-    #     return lines
+
+    def render(self, ax):
+        # center of the box
+
+        colors = ['red', 'blue', 'green']
+        static_axes = np.eye(3)
+
+        lines = []
+
+        for i in range(3):
+            # fixed coordinate system
+            vector = np.array([self.center, self.center + static_axes[i]]).T
+            lines.append(ax.plot(vector[0], vector[1], vector[2], color=colors[i])[0])
+
+            # coordinate system associated with the box
+            vector = np.array([self.center, self.center + self.matrix[i]]).T
+            lines.append(ax.plot(vector[0], vector[1], vector[2], color=colors[i])[0])
+
+        # box
+        for edge in self.box_edges():
+            lines.append(ax.plot(edge[0], edge[1], edge[2], color='grey')[0])
+
+        return lines
 
     def apply_control(self, v, omega, dt):
         self.center = self.center + v * dt
@@ -74,101 +76,56 @@ class Box:
 
 
 class Point:
-    def __init__(self, location):
-        self.location = location
+    def __init__(self, position):
+        self.position = position
 
-    # def render(self, ax, color='black', size=5):
-    #     return [ax.plot([self.location[0]], [self.location[1]],
-    #                     [self.location[2]], color=color,
-    #                     marker="o", markersize=size)]
+    def render(self, ax):
+        return ax.plot([self.position[0]], [self.position[1]],
+                       [self.position[2]], color='black',
+                       marker="o", markersize=5)
 
-    def apply_control(self, v, dt):
-        self.location = self.location + v * dt
+    def apply_control(self, is_linear, velocity, dt):
+        if is_linear:
+            # velocity is linear
+            self.position = self.position + velocity * dt
+        else:
+            # velocity is angular velocity of the box
+            self.position = R.from_rotvec(velocity * dt).as_matrix() @ self.position
 
 
 class System:
     def __init__(self, box, points):
         self.box = box
-        self.points = np.array(points)
+        self.points = points
 
     def render(self, ax):
-        colors = ['red', 'blue', 'green']
-        static_axes = np.eye(3)
-
-        lines = []
-        box = self.box
-        for i in range(3):
-            # fixed coordinate system
-            vector = np.array([box.center, box.center + static_axes[i]]).T
-            lines.append(ax.plot(vector[0], vector[1], vector[2], color=colors[i])[0])
-
-            # coordinate system associated with the body
-            vector = np.array([box.center, box.center + box.matrix[i]]).T
-            lines.append(ax.plot(vector[0], vector[1], vector[2], color=colors[i])[0])
-
-        # body
-        for edge in box.box_edges():
-            lines.append(ax.plot(edge[0], edge[1], edge[2], color='grey')[0])
-
+        lines = self.box.render(ax)
         for point in self.points:
-            lines.append(ax.plot([point.position[0]], [point.position[1]],
-                                 [point.position[2]], color='black',
-                                 marker="o", markersize=5))
-
+            lines.append(point.render(ax))
         return lines
 
     def apply_control(self, v_box, omega_box, v_points, dt):
         self.box.apply_control(v_box, omega_box, dt)
-        [point.apply_control(v_point, dt)
-         for (point, v_point) in zip(self.points, v_points)]
+        for (point, v_point) in zip(self.points, v_points):
+            if np.linalg.norm(v_point):
+                # point with its own motion
+                point.apply_control(True, v_point, dt)
+            else:
+                # point without its own motion - rotates with the box
+                point.apply_control(False, omega_box, dt)
 
 
 def update_system(phase_number, system, dt, ax):
     ax.lines.clear()
     v_box = np.zeros(3)
-    i = float(0 < phase_number <= 100)
-    j = float(100 < phase_number <= 200)
-    k = float(200 < phase_number <= 300)
-    omega_box = np.array([i, j, k]) * np.pi/2
     v_points = np.zeros((len(system.points), 3))
-    dtheta = np.pi/2 * dt
-    center = system.box.center
-    if i:
-        initial_position = system.points[2].position - center
-        v_yz = (np.array([initial_position[2],
-                          -initial_position[1]]) * np.sin(dtheta) -
-                np.array([initial_position[1],
-                          initial_position[2]]) * (1 - np.cos(dtheta)))
-        v_yz = v_yz / np.linalg.norm(v_yz) * np.linalg.norm(initial_position)
-        v = np.array([initial_position[0],
-                      v_yz[0],
-                      v_yz[1]])
-        v_points[2] = v
-        v_points[3] = -v
-    if j:
-        initial_position = system.points[4].position - center
-        v_zx = (np.array([initial_position[0],
-                          -initial_position[2]]) * np.sin(dtheta) -
-                np.array([initial_position[2],
-                          initial_position[0]]) * (1 - np.cos(dtheta)))
-        v_zx = v_zx / np.linalg.norm(v_zx) * np.linalg.norm(initial_position)
-        v = np.array([v_zx[1],
-                      initial_position[0],
-                      v_zx[0]])
-        v_points[4] = v
-        v_points[5] = -v
-    if k:
-        initial_position = system.points[0].position - center
-        v_xy = (np.array([initial_position[1],
-                          -initial_position[0]]) * np.sin(dtheta) -
-                np.array([initial_position[0],
-                          initial_position[1]]) * (1 - np.cos(dtheta)))
-        v_xy = v_xy / np.linalg.norm(v_xy) * np.linalg.norm(initial_position)
-        v = np.array([v_xy[0],
-                      v_xy[1],
-                      initial_position[0]])
-        v_points[0] = v
-        v_points[1] = -v
+    omega_box = np.zeros(3)
+    if 0 < phase_number <= 100:
+        omega_box = np.array([np.pi/2, 0, 0])
+    elif 100 < phase_number <= 200:
+        omega_box = np.array([0, np.pi/2, 0])
+    elif 200 < phase_number <= 300:
+        omega_box = np.array([0, 0, np.pi/2])
     system.apply_control(v_box, omega_box, v_points, dt)
     return [system.render(ax)]
 
@@ -201,7 +158,6 @@ if __name__ == "__main__":
                                               [0.0, 0.0, -4.0]])])
 
     system = System(cube, points)
-    system.render(ax)
 
     n = 301
     dt = 0.01
