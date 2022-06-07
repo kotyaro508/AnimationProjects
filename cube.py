edfdc2fed218fbac3730e81070ca79f77d04998a
kotyaro508 <kotliarov.iad@phystech.edu>
2022-06-07 09:06:44 +0300

    First reformation

--- cube.py
+++ cube.py
@@ -8,37 +8,43 @@ class Box:
     def __init__(self, center, sizes, orientation):
         self.center = center
         self.sizes = sizes
-        self.matrix = R.from_euler('xyz', orientation).as_matrix().T
+        self.matrix = R.from_euler("xyz", orientation).as_matrix().T
 
     def box_edges(self):
         edges = []
 
         for j in (-1, 1):
             for k in (-1, 1):
-                edges.append(np.array([[-self.sizes[0],
-                                        j * self.sizes[1],
-                                        k * self.sizes[2]],
-                                       [self.sizes[0],
-                                        j * self.sizes[1],
-                                        k * self.sizes[2]]]).T)
+                edges.append(
+                    np.array(
+                        [
+                            [-self.sizes[0], j * self.sizes[1], k * self.sizes[2]],
+                            [self.sizes[0], j * self.sizes[1], k * self.sizes[2]],
+                        ]
+                    ).T
+                )
 
         for k in (-1, 1):
             for i in (-1, 1):
-                edges.append(np.array([[i * self.sizes[0],
-                                        -self.sizes[1],
-                                        k * self.sizes[2]],
-                                       [i * self.sizes[0],
-                                        self.sizes[1],
-                                        k * self.sizes[2]]]).T)
+                edges.append(
+                    np.array(
+                        [
+                            [i * self.sizes[0], -self.sizes[1], k * self.sizes[2]],
+                            [i * self.sizes[0], self.sizes[1], k * self.sizes[2]],
+                        ]
+                    ).T
+                )
 
         for i in (-1, 1):
             for j in (-1, 1):
-                edges.append(np.array([[i * self.sizes[0],
-                                        j * self.sizes[1],
-                                        -self.sizes[2]],
-                                       [i * self.sizes[0],
-                                        j * self.sizes[1],
-                                        self.sizes[2]]]).T)
+                edges.append(
+                    np.array(
+                        [
+                            [i * self.sizes[0], j * self.sizes[1], -self.sizes[2]],
+                            [i * self.sizes[0], j * self.sizes[1], self.sizes[2]],
+                        ]
+                    ).T
+                )
 
         edges = np.array(edges) / 2
 
@@ -50,7 +56,7 @@ class Box:
     def render(self, ax):
         # center of the box
 
-        colors = ['red', 'blue', 'green']
+        colors = ["red", "blue", "green"]
         static_axes = np.eye(3)
 
         lines = []
@@ -66,7 +72,7 @@ class Box:
 
         # box
         for edge in self.box_edges():
-            lines.append(ax.plot(edge[0], edge[1], edge[2], color='grey')[0])
+            lines.append(ax.plot(edge[0], edge[1], edge[2], color="grey")[0])
 
         return lines
 
@@ -80,9 +86,14 @@ class Point:
         self.position = position
 
     def render(self, ax):
-        return ax.plot([self.position[0]], [self.position[1]],
-                       [self.position[2]], color='black',
-                       marker="o", markersize=5)
+        return ax.plot(
+            [self.position[0]],
+            [self.position[1]],
+            [self.position[2]],
+            color="black",
+            marker="o",
+            markersize=5,
+        )
 
     def apply_control(self, is_linear, velocity, dt):
         if is_linear:
@@ -121,11 +132,11 @@ def update_system(phase_number, system, dt, ax):
     v_points = np.zeros((len(system.points), 3))
     omega_box = np.zeros(3)
     if 0 < phase_number <= 100:
-        omega_box = np.array([np.pi/2, 0, 0])
+        omega_box = np.array([np.pi / 2, 0, 0])
     elif 100 < phase_number <= 200:
-        omega_box = np.array([0, np.pi/2, 0])
+        omega_box = np.array([0, np.pi / 2, 0])
     elif 200 < phase_number <= 300:
-        omega_box = np.array([0, 0, np.pi/2])
+        omega_box = np.array([0, 0, np.pi / 2])
     system.apply_control(v_box, omega_box, v_points, dt)
     return [system.render(ax)]
 
@@ -142,20 +153,30 @@ if __name__ == "__main__":
 
     limits = []
     for i in range(3):
-        limits.append(np.array([box_center[i], box_center[i]]) +
-                      np.array([-max(box_sizes), max(box_sizes)]) * 0.7)
+        limits.append(
+            np.array([box_center[i], box_center[i]])
+            + np.array([-max(box_sizes), max(box_sizes)]) * 0.7
+        )
 
-    ax.set(xlim3d=limits[0], xlabel='X')
-    ax.set(ylim3d=limits[1], ylabel='Y')
-    ax.set(zlim3d=limits[2], zlabel='Z')
+    ax.set(xlim3d=limits[0], xlabel="X")
+    ax.set(ylim3d=limits[1], ylabel="Y")
+    ax.set(zlim3d=limits[2], zlabel="Z")
 
-    points = np.array([Point(point + box_center)
-                       for point in np.array([[1.0, 0.0, 0.0],
-                                              [-1.0, 0.0, 0.0],
-                                              [0.0, 2.0, 0.0],
-                                              [0.0, -2.0, 0.0],
-                                              [0.0, 0.0, 4.0],
-                                              [0.0, 0.0, -4.0]])])
+    points = np.array(
+        [
+            Point(point + box_center)
+            for point in np.array(
+                [
+                    [1.0, 0.0, 0.0],
+                    [-1.0, 0.0, 0.0],
+                    [0.0, 2.0, 0.0],
+                    [0.0, -2.0, 0.0],
+                    [0.0, 0.0, 4.0],
+                    [0.0, 0.0, -4.0],
+                ]
+            )
+        ]
+    )
 
     system = System(cube, points)
 
@@ -165,11 +186,13 @@ if __name__ == "__main__":
 
     fps = 10
 
-    animation = FuncAnimation(fig=fig,
-                              func=update_system,
-                              frames=phase,
-                              fargs=(system, dt, ax),
-                              interval=1000 / fps,
-                              repeat=False)
-    fn = 'cube_rotation_funcanimation'
-    animation.save(fn + '.html', writer='html', fps=fps)
+    animation = FuncAnimation(
+        fig=fig,
+        func=update_system,
+        frames=phase,
+        fargs=(system, dt, ax),
+        interval=1000 / fps,
+        repeat=False,
+    )
+    fn = "cube_rotation_funcanimation"
+    animation.save(fn + ".html", writer="html", fps=fps)
